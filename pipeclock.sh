078274b833146aa7d4053a50520ef550301c799e
Cameron Reynolds <starlordv125@protonmail.com>
2025-09-18 22:33:18 -0400

    Added new function

--- pipeclock.sh
+++ pipeclock.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+pcVersion="0.1-alpha" 
 # Function that checks for pipewire installation
 function pipewire_check() {
     # Checks for pipewire installation
@@ -14,7 +15,8 @@ function pipewire_check() {
     fi
 }
 
-function file_check() {
+function folder_make() {
+    echo "Making Pipewire config folder"
     if [ ! -d "$HOME/.config" ]; then
         mkdir "$HOME/.config"
     fi
@@ -23,11 +25,92 @@ function file_check() {
         mkdir "$HOME/.config/pipewire"
     fi
 
-    if [ -f "$HOME/.config/pipewire/pipewire.conf" ]; then
-        echo "WORKS"
-    else
-        touch "$HOME/.config/pipewire/pipewire.conf"
+    if [ ! -d "$HOME/.config/pipewire/pipewire.conf.d" ]; then
+        mkdir "$HOME/.config/pipewire/pipewire.conf.d"
     fi
 }
+
+function make_conf() {
+    echo 
+    touch clock.conf
+
+}
+
+function clock_preferences() {
+    echo -e "Welcome to Pipeclock v$pcVersion!"
+    while true; do
+        echo -e "\nPlease select minimum clock speed"
+        echo "1. 44100
+2. 48000
+3. 88200
+4. 96000
+5. 192000
+** (1-5, 0 to abort) **
+"
+        read minClock
+        if [ "$minClock" -eq "0" ]; then
+            echo -e "\nExiting Pipeclock v$pcVersion"
+            exit 1
+        elif [ "$minClock" -eq "1" ]; then
+            minClock=44100
+            break
+        elif [ "$minClock" -eq "2" ]; then
+            minClock=48000
+            break
+        elif [ "$minClock" -eq "3" ]; then
+            minClock=88200
+            break
+        elif [ "$minClock" -eq "4" ]; then
+            minClock=96000
+            break
+        elif [ "$minClock" -eq "5" ]; then
+            minClock=192000
+            break
+        else
+            echo -e "\nError: Please enter a number that is 0-5"
+        fi
+    done
+    echo -e "\nYou have chosen $minClock as your minimum clock rate"
+    while true; do
+        echo -e "\nPlease select maximum clock speed"
+        echo "1. 44100
+2. 48000
+3. 88200
+4. 96000
+5. 192000
+** (1-5, 0 to abort) **
+"
+        read maxClock
+        if [ "$maxClock" -eq "0" ]; then
+            echo -e "\nExiting Pipeclock v$pcVersion"
+            exit 1
+        elif [ "$maxClock" -eq "1" ]; then
+            maxClock=44100
+            break
+        elif [ "$maxClock" -eq "2" ]; then
+            maxClock=48000
+            break
+        elif [ "$maxClock" -eq "3" ]; then
+            maxClock=88200
+            break
+        elif [ "$maxClock" -eq "4" ]; then
+            maxClock=96000
+            break
+        elif [ "$maxClock" -eq "5" ]; then
+            maxClock=192000
+            break
+        else
+            echo -e "\nError: Please enter a number that is 0-5"
+        fi
+    done
+    echo -e "\nYou have chosen $maxClock as your maximum clock rate"
+}
+
 pipewire_check
-file_check
+clock_preferences
+folder_make
+#if [ ! -e "$HOME/.config/pipewire/pipewire.conf.d/clock.conf" ]; then
+#    make_conf
+#elif [ -e "$HOME/.config/pipewire/pipewire.conf.d/clock.conf" ]; then
+#    edit_conf
+#fi
